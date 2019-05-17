2de751f56d863581082a398e121f63fa83f04204
weiguangju__1994@163.com <weiguangju_1994@163.com>
2019-05-18 02:14:24 +0800

    setOnClick on FruitListView

--- app/src/main/java/com/example/wgj/listviewtest/MainActivity.java
+++ app/src/main/java/com/example/wgj/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.wgj.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void initFruits() {
